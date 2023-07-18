e0e064680dbe6824aeafdcd992a20f6741295ef9
J34993R <guialves.oliveira1234@gmail.com>
2023-07-18 15:51:29 -0300

    Created topbar component.

---
+++ view/src/components/topbar/Topbar.js
@@ -0,0 +1,39 @@
+import './topbar.css'
+import {Person, Search, Chat, Notifications} from '@mui/icons-material'
+
+export default function Topbar() {
+    return (
+        <div className="topbarContainer">
+            <div className='topbarLeft'>
+                <span className='logo'>Alvesocial</span>
+            </div>
+            <div className='topbarCenter'>
+                <div className='searchbar'>
+                    <Search className='searchIcon'/>
+                    <input className='searchInput' placeholder='Search for anything'/>
+                </div>
+            </div>
+            <div className='topbarRight'>
+                <div className='topbarLinks'>
+                    <span>Homepage</span>
+                    <span>Timeline</span>
+                </div>
+                <div className='topbarIcons'>
+                    <div className='topbarIconItem'>
+                        <Person/>
+                        <span className='topbarIconBadge'>1</span>
+                    </div>
+                    <div className='topbarIconItem'>
+                        <Chat/>
+                        <span className='topbarIconBadge'>99+</span>
+                    </div>
+                    <div className='topbarIconItem'>
+                        <Notifications/>
+                        <span className='topbarIconBadge'>99+</span>
+                    </div>
+                </div>
+                <img className='topbarImg' alt='' src='/assets/person/1.jpeg' />
+            </div>
+        </div>
+    )
+}

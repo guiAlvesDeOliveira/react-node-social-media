1bdfa7f2e52d799e3b47f0186c5c604d4cf75103
J34993R <guialves.oliveira1234@gmail.com>
2023-08-03 15:34:49 -0300

    Share function. Upload images.

--- view/src/pages/profile/Profile.js
+++ view/src/pages/profile/Profile.js
@@ -28,8 +28,8 @@ export default function Profile() {
                     <div className='profileRightTop'>
                         <div className='profileCover'>
 
-                            <img src={user.coverPicture || PF+'person/noCover.png'} alt='' className='profileCoverImg'/>
-                            <img src={user.profilePicture || PF+'person/noAvatar.png'} alt='' className='profileUserImg'/>
+                            <img src={user.coverPicture ? PF+user.coverPicture : PF+'person/noCover.png'} alt='' className='profileCoverImg'/>
+                            <img src={user.profilePicture ? PF+user.profilePicture : PF+'person/noAvatar.png'} alt='' className='profileUserImg'/>
                         </div>
                         <div className='profileInfo'>
                             <h4 className='profileInfoName'>{user.username}</h4>

1423a566f67c46b73f6e94f22f48d0e5d73e8efe
J34993R <guialves.oliveira1234@gmail.com>
2023-08-03 08:20:28 -0300

    Fetching api with axios, added timeago.js.

--- view/src/components/post/Post.js
+++ view/src/components/post/Post.js
@@ -1,28 +1,38 @@
 import './post.css';
 import {MoreVert} from "@mui/icons-material";
-import {Users} from "../../dummyData";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import {format} from 'timeago.js';
 
 export default function Post({post}) {
     const PF = process.env.REACT_APP_PUBLIC_FOLDER;
-    const [like, setLike] = useState(post.like);
+    const [like, setLike] = useState(post.likes.length);
     const [isLiked, setIsLiked] = useState(false);
+    const [user, setUser] = useState({});
 
 
     const likeHandler = () => {
-        setLike(isLiked ? like -1 : like+1);
+        setLike(isLiked ? like - 1 : like + 1);
         setIsLiked(!isLiked)
     }
+
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = await axios.get(`/users/${post.userId}`)
+            setUser(res.data)
+        };
+        fetchUser();
+    }, [post.userId]);
 
     return (
         <div className='post'>
             <div className='postWrapper'>
                 <div className='postTop'>
                     <div className='postTopLeft'>
-                        <img src={Users.filter(u => u.id === post.userId)[0].profilePicture} alt=''
+                        <img src={user.profilePicture || PF + 'person/noAvatar.png'} alt=''
                              className='postProfileImg'/>
-                        <span className='postUsername'>{Users.filter(u => u.id === post.userId)[0].username}</span>
-                        <span className='postDate'>{post.date}</span>
+                        <span className='postUsername'>{user.username}</span>
+                        <span className='postDate'>{format(post.createdAt)}</span>
                     </div>
                     <div className='postTopRight'>
                         <MoreVert/>
@@ -30,7 +40,7 @@ export default function Post({post}) {
                 </div>
                 <div className='postCenter'>
                     <span className='postText'>{post?.desc}</span>
-                    <img src={PF+post.photo} alt='' className='postImg'/>
+                    <img src={PF + post.img} alt='' className='postImg'/>
                 </div>
                 <div className='postBottom'>
                     <div className='postBottomLeft'>

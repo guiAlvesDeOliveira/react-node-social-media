d0de249c8f781c5c407bb255d4e66aecb1b7d9a9
J34993R <guialves.oliveira1234@gmail.com>
2023-08-03 12:51:24 -0300

    Downgrade react-router-dom to version 5.2.0. Login function.

--- view/src/pages/login/Login.js
+++ view/src/pages/login/Login.js
@@ -1,13 +1,20 @@
 import './login.css';
-import {useRef} from "react";
+import {useContext, useRef} from "react";
+import {loginCall} from "../../apiCalls";
+import {AuthContext} from "../../context/AuthContext";
+import {CircularProgress} from "@mui/material";
 
 export default function Login() {
     const email = useRef();
     const password = useRef();
+    const {user, isFetching, error, dispatch} = useContext(AuthContext)
+
     const handleClick = (e) => {
         e.preventDefault();
-        console.log('clicked');
+        loginCall({email: email.current.value, password: password.current.value}, dispatch)
     }
+
+    console.log(user)
     return (
         <div className='login'>
             <div className='loginWrapper'>
@@ -18,10 +25,13 @@ export default function Login() {
                 <form className='loginRight' onSubmit={handleClick}>
                     <div className='loginBox'>
                         <input className='loginInput' type='email' placeholder='Email' ref={email} required/>
-                        <input className='loginInput' type='password' placeholder='Password' ref={password} required minLength='6'/>
-                        <button className='loginButton'>Log In</button>
+                        <input className='loginInput' type='password' placeholder='Password' ref={password} required
+                               minLength='6'/>
+                        <button className='loginButton' disabled={isFetching}>{isFetching ?
+                            <CircularProgress color="secondary" size='15px'/> : 'Log In'}</button>
                         <span className='loginForgot'>Forgot Password?</span>
-                        <button className='loginRegisterButton'>Register</button>
+                        <button className='loginRegisterButton' disabled={isFetching}>{isFetching ?
+                            <CircularProgress color="secondary" size='15px'/> : 'Register'}</button>
                     </div>
                 </form>
             </div>
